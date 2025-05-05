513ee950dacfd79a5a3dcf51149bef74b4226b99
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2025-05-05 15:45:55 +0300

    v 0.03

---
+++ scripts/player/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var max_speed = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var movement = movement_vector()
+	var direction = movement.normalized()
+	velocity = max_speed * direction
+	move_and_slide()
+
+func movement_vector():
+	var movement_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var movement_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(movement_x, movement_y)
