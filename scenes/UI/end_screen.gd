40f1ae5bca7163c4d2c31e811012df4e8970de3f
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2025-05-11 21:47:55 +0300

    v 0.45

--- scenes/UI/end_screen.gd
+++ scenes/UI/end_screen.gd
@@ -2,8 +2,14 @@ extends CanvasLayer
 class_name EndScreen
 
 @onready var name_label: Label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/NameLabel
+@onready var panel_container: PanelContainer = $MarginContainer/PanelContainer
 
 func _ready() -> void:
+	panel_container.pivot_offset = panel_container.size / 2
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 	get_tree().paused = true
 
 func change_to_victory():
