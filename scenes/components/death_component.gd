7ce44beb1fe40d1f667ceba73277bd98869596ae
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2025-05-11 23:15:02 +0300

    v 0.49

--- scenes/components/death_component.gd
+++ scenes/components/death_component.gd
@@ -3,3 +3,7 @@ class_name DeathComponent
 
 @onready var gpu_particles_2d: GPUParticles2D = $SpriteOffset/GPUParticles2D
 @onready var sprite_offset: Node2D = $SpriteOffset
+
+func _ready() -> void:
+	$HitSoundComponent.play()
+	$AudioStreamPlayer2D.play()
--- scenes/components/hurt_box_component.gd
+++ scenes/components/hurt_box_component.gd
@@ -1,7 +1,8 @@
 extends Area2D
 class_name HurtBoxComponent
 
-@export var health_component: HealthComponent 
+@export var health_component: HealthComponent
+@export var hit_sound_component: AudioStreamPlayer2D
 
 func _on_area_entered(area: Area2D) -> void:
 	if not area is HitBoxComponent:
@@ -10,3 +11,4 @@ func _on_area_entered(area: Area2D) -> void:
 		return
 	var hit_box_component = area as HitBoxComponent
 	health_component.take_damage(hit_box_component.damage)
+	hit_sound_component.play()
