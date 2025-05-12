6c8620f99412635b31212d4c2c80c940f6f8ed94
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2025-05-12 10:45:42 +0300

    v 0.52

--- scenes/UI/main_menu.gd
+++ scenes/UI/main_menu.gd
@@ -1,13 +1,16 @@
 extends CanvasLayer
 
+var options_menu_scene = preload("res://scenes/UI/options_menu.tscn")
+
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
 
 
 func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
-
+	print("Options")
+	var options_instance = options_menu_scene.instantiate()
+	add_child(options_instance)
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
