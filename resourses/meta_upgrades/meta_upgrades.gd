170406b9692367b8bde1b6b6064ba6d76d45ed10
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2025-05-12 17:16:47 +0300

    v 0.54

---
+++ resourses/meta_upgrades/meta_upgrades.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name MetaUpgrades
+
+
+@export var id: String
+@export var max_quantity: int = 1
+@export var name: String
+@export_multiline var description: String
---
+++ scenes/global/meta_progression.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var save_data: Dictionary = {
+	"meta_upgrade_currency": 0,
+	"meta_upgrades": {}
+}
+
+func _ready() -> void:
+	Global.experienced_bottle_collected.connect(on_experience_collected)
+	add_meta_upgrade(load("res://resourses/meta_upgrades/experience_drop_chance.tres"))
+
+
+func add_meta_upgrade(upgrade: MetaUpgrades):
+	if not save_data["meta_upgrades"].has(upgrade.id):
+		save_data["meta_upgrades"][upgrade.id] = {
+			"quantity": 0
+		}
+		save_data["meta_upgrades"][upgrade.id]["quantity"] += 1
+		print(save_data)
+
+
+func on_experience_collected(number: float):
+	save_data["meta_upgrade_currency"] += number
