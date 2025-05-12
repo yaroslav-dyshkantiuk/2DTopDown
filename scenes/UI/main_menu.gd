447a0000163cdc9c6347fdc0267abc33b9b68be2
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2025-05-12 07:18:24 +0300

    v 0.51

---
+++ scenes/UI/main_menu.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
