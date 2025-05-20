6600d5761c081ebb7233b0b52a502c8d2f5314e4
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2025-05-20 14:16:11 +0300

    v 0.55

--- scenes/global/meta_progression.gd
+++ scenes/global/meta_progression.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var save_path = "user://game.save"
+
 var save_data: Dictionary = {
 	"meta_upgrade_currency": 0,
 	"meta_upgrades": {}
@@ -8,6 +10,19 @@ var save_data: Dictionary = {
 func _ready() -> void:
 	Global.experienced_bottle_collected.connect(on_experience_collected)
 	add_meta_upgrade(load("res://resourses/meta_upgrades/experience_drop_chance.tres"))
+	load_file()
+
+
+func save_file():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var(save_data)
+
+
+func load_file():
+	if not FileAccess.file_exists(save_path):
+		return
+	var file = FileAccess.open(save_path, FileAccess.READ)
+	save_data = file.get_var()
 
 
 func add_meta_upgrade(upgrade: MetaUpgrades):
@@ -21,3 +36,4 @@ func add_meta_upgrade(upgrade: MetaUpgrades):
 
 func on_experience_collected(number: float):
 	save_data["meta_upgrade_currency"] += number
+	save_file()
